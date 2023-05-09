60a8faf60500734192798e68a3834735f25a2dd8
Milan Patel <patelmilan020@gmail.com>
2023-05-09 08:14:37 +0530

    Update users.js

--- routers/users.js
+++ routers/users.js
@@ -108,7 +108,7 @@ router.post("/login", async (req, res) => {
   const secret = process.env.secret;
 
   if (!user) {
-    return res.status(400).send("The user not found !!!");
+    return res.status(400).send(message:"The user not found !!!");
   }
 
   if (user && bcrypt.compareSync(req.body.password, user.password)) {
